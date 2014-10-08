7550b153f2eda424a729b7e08a87240c79d68951
Wenhao Chen <xjtujack@gmail.com>
2014-10-08 00:11:23 -0500

    smali grammar updating

--- src/analysisTools/SmaliGrammar.java
+++ src/analysisTools/SmaliGrammar.java
@@ -2,6 +2,19 @@ package analysisTools;
 
 public class SmaliGrammar {
 
+	public static final String[] poundHeads = {
+		"# interfaces",
+		"# annotations",
+		"# instance fields",
+		"# static fields",
+		"# direct methods",
+		"# virtual methods",
+		"#calls:",
+		"getter for:",
+		"setter for:",
+		
+	};
+	
 	public static final String[] labelHeads = {
 		":cond",
 		":goto",
@@ -11,9 +24,17 @@ public class SmaliGrammar {
 	};
 	
 	public static final String[] dotHeads = {
-		".class", ".super", ".source", ".implements",
-		".annotation", 
-		
+		".class", ".super", ".source", ".implements", ".enum", 
+		".annotation", ".end annotation", ".subannotation", ".end subannotation", 
+		".field", ".end field",
+		".method", ".end method", ".prologue", ".line", 
+		".locals", ".local", ".end local", 
+		".parameter", ".end parameter", 
+		".catchall", ".catch", 
+		".restart",
+		".sparse-switch", ".end sparse-switch", 
+		".packed-switch", ".end packed-switch", 
+		".array-data", ".end array-data", 
 	};
 	
 }
